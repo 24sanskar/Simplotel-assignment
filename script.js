59e6ff4b707de3d606884b40f547fd9f07ddcba3
24sanskar <105884159+24sanskar@users.noreply.github.com>
2023-05-16 15:07:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navBar = document.querySelector(".nav-bar");
-
-hamburger.addEventListener("click", () => {
-  navBar.classList.toggle("active");
-});
